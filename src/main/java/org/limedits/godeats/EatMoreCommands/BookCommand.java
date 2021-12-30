fcf1d744cbdc8aefa643fcdafd58f40c1fe2a232
Ava <dtyasinmax@gmail.com>
2021-12-29 21:55:22 -0600

    Found a way to add Meta to the Book

--- src/main/java/org/limedits/godeats/EatMoreCommands/BookCommand.java
+++ src/main/java/org/limedits/godeats/EatMoreCommands/BookCommand.java
@@ -1,19 +1,31 @@
 package org.limedits.godeats.EatMoreCommands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+import java.util.Collections;
 
 public class BookCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player) sender;
-        Material item = Material.WRITTEN_BOOK;
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta bookMeta  = (BookMeta) book.getItemMeta();
+        bookMeta.setAuthor("Ava");
+        bookMeta.setDisplayName("Eat More Guide");
+        bookMeta.setLore(Collections.singletonList(ChatColor.LIGHT_PURPLE + "Eat More guide for the less Intelligent"));
+        bookMeta.getLore();
+        book.setItemMeta(bookMeta);
+
         if(sender instanceof Player){
-            p.getInventory().addItem(new ItemStack(item));
+            p.getInventory().addItem(new ItemStack(book));
         }else {
             System.out.println("You must be a player to use this command");
             return false;
@@ -22,4 +34,7 @@ public class BookCommand implements CommandExecutor {
 
         return true;
     }
+
+
+
 }

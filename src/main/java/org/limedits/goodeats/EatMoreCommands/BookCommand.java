390732e6a48149178efa8b9bf16bf06a16c8f6fc
Ava <dtyasinmax@gmail.com>
2021-12-29 23:19:13 -0600

    fixed

--- src/main/java/org/limedits/goodeats/EatMoreCommands/BookCommand.java
+++ src/main/java/org/limedits/goodeats/EatMoreCommands/BookCommand.java
@@ -13,23 +13,19 @@ import java.util.Collections;
 
 public class BookCommand implements CommandExecutor {
 
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player) sender;
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta bookMeta  = (BookMeta) book.getItemMeta();
         bookMeta.setAuthor("Ava");
-        bookMeta.setDisplayName("Eat More Guide");
+        bookMeta.setDisplayName(ChatColor.DARK_RED +  "Eat More Guide");
         bookMeta.isUnbreakable();
-        try {
-            book.wait(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         bookMeta.setLore(Collections.singletonList(ChatColor.LIGHT_PURPLE + "Eat More guide for the less Intelligent"));
         bookMeta.getLore();
         book.setItemMeta(bookMeta);
-
         if(sender instanceof Player){
             p.getInventory().addItem(new ItemStack(book));
         }else {

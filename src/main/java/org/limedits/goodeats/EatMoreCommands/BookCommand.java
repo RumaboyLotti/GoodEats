592a1e610e4dff7c341630c725742d71bad8048a
Ava <dtyasinmax@gmail.com>
2022-01-10 02:36:07 -0600

    typo fixed

--- src/main/java/org/limedits/goodeats/EatMoreCommands/BookCommand.java
+++ src/main/java/org/limedits/goodeats/EatMoreCommands/BookCommand.java
@@ -53,8 +53,8 @@ public class BookCommand implements CommandExecutor {
             p.getInventory().addItem(new ItemStack(book));
             if(cooldown.containsKey(p.getName())){
                 if(cooldown.get(p.getName()) > System.currentTimeMillis()){
-                    long timeleft = (cooldown.get(p.getName()) - System.currentTimeMillis()) /1000;
-                    p.sendMessage(ChatColor.GOLD + "Calm down Buck a roo" + timeleft + "Seconds");
+                    long time-left = (cooldown.get(p.getName()) - System.currentTimeMillis()) /1000;
+                    p.sendMessage(ChatColor.GOLD + "Calm down Buck a roo" + time-left + "Seconds");
                     System.out.println();
                 }
                 return true;
